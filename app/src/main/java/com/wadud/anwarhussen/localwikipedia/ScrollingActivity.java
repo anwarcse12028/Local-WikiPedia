a4eb44d01a5c729b75f1cc7300e7e262b53dcb9e
Anwar Hussen Wadud <mahwadud@gmail.com>
2016-12-04 19:12:35 +0600

    Local Wikipedia
    Local wikipedia is used to read wiki file on local language

---
+++ app/src/main/java/com/wadud/anwarhussen/localwikipedia/ScrollingActivity.java
@@ -0,0 +1,107 @@
+package com.wadud.anwarhussen.localwikipedia;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    WebView myWebView;
+    String website;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        myWebView = (WebView)findViewById(R.id.webview);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        String loc = getResources().getString(R.string.local);
+        website = "http://"+loc+".wikipedia.org/wiki";
+
+        // Configure related browser settings
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        myWebView.setWebViewClient(new MyBrowser(progressBar));
+        // Load the initial URL
+        myWebView.loadUrl(website);
+
+    }
+    // Manages the behavior when URLs are loaded
+    private class MyBrowser extends WebViewClient {
+
+        private ProgressBar progressBar;
+        MyBrowser(ProgressBar progressBar){
+            this.progressBar = progressBar;
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.action_aboutme){
+            Toast.makeText(getApplicationContext(),getResources().getString(R.string.dev_name),Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
